24108de4853171d8cfbf5cc95f3c044e1a817066
akshatgupta105 <53713926+Dev-AkshatGupta@users.noreply.github.com>
2022-09-24 22:39:56 +0530

    fix : logic for the closest lift

--- script.js
+++ script.js
@@ -3,55 +3,48 @@ const liftsNumber = document.querySelector("#lifts");
 const floorsNumber = document.querySelector("#floors");
 const simulationDiv = document.querySelector(".simulation");
 let liftBtn;
-let closestLift = Infinity;
 let lifts;
+let liftToCall;
+
 simulateBtn.addEventListener("click", () => {
   // For floors
-  for (let i = 1; i <= floorsNumber.value; i++) {
-    let divEl = document.createElement("div");
-    let button = document.createElement("button");
-    button.textContent = "call ";
-    button.className = "btn";
-    button.dataset.floor = i;
-    divEl.className = "floor";
-    simulationDiv.appendChild(divEl);
-    divEl.appendChild(button);
-  }
 
-  liftBtn = document.querySelectorAll(".btn");
   // for lifts
+
   for (let i = 1; i <= liftsNumber.value; i++) {
     let divEl = document.createElement("div");
     divEl.className = "lifts";
     divEl.dataset.floor = 0;
     divEl.dataset.liftNumber = i;
+    divEl.style.left = `${i * 40 + i}px`;
     simulationDiv.appendChild(divEl);
   }
+
+  liftBtn = document.querySelectorAll(".btn");
   lifts = document.querySelectorAll(".lifts");
-  console.log(lifts[0].dataset.floor);
-  //   for floor buttons for calling lifts
-  console.log(liftBtn);
-  let currentLiftValue = Infinity;
-  let liftToCall;
-  liftBtn.forEach((button, index) =>
+
+  // floor buttons for calling lifts
+
+  liftBtn.forEach((button, index) => {
     button.addEventListener("click", () => {
-      // console.log(index);
+      // loop for finding the closest lift
+      let closestLiftFloorDifference = 10;
       lifts.forEach((lift, idx) => {
         if (
-          Number.parseInt(button.dataset.floor - lift.dataset.floor) <
-          currentLiftValue
+          Math.abs(+button.dataset.floor - +lift.dataset.floor) <
+          +closestLiftFloorDifference
         ) {
-          currentLiftValue = Number.parseInt(
-            button.dataset.floor - lift.dataset.floor
+          closestLiftFloorDifference = Math.abs(
+            +button.dataset.floor - +lift.dataset.floor
           );
           liftToCall = lift;
-          console.log(liftToCall);
         }
-        console.log({ currentLiftValue, liftToCall });
       });
-      if (currentLiftValue) {
-      }
-      // currentLiftValue=index-liftsNumber
-    })
-  );
+      // liftToCall is closest lift
+      liftToCall.dataset.floor = button.dataset.floor;
+      liftToCall.style.bottom = `${+liftToCall.dataset.floor * 70 - 70}px`;
+    });
+  });
 });
+
+function createFloors(floorsNumber) {}
